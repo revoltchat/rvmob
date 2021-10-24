bb0b6fce623cb154163b109f1698f87b210a11c0
TaiAurori <31465218+TaiAurori@users.noreply.github.com>
2021-10-24 14:57:20 -0400

    split themeing into another file

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import 'react-native-get-random-values' // react native moment
 import './shim' // react native moment 2
-
 import React from 'react';
 import { StyleSheet, View, TouchableOpacity, Pressable, Modal, ScrollView, TextInput, StatusBar, Dimensions, Linking, FlatList } from 'react-native';
 import FastImage from 'react-native-fast-image'
@@ -18,6 +17,7 @@ import { decodeTime } from 'ulid'
 import ImageViewer from 'react-native-image-zoom-viewer';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import ConfirmHcaptcha from '@hcaptcha/react-native-hcaptcha';
+import { setTheme, currentTheme, currentThemeName, styles, themes } from './src/Theme'
 
 const client = new Client();
 
@@ -406,7 +406,10 @@ class MainView extends React.Component {
                             <ScrollView style={{flex: 1}}>
                                 <Text>Change Theme</Text>
                                 <ScrollView style={{height: "30%", width: "90%", margin: 10, padding: 10 - currentTheme.generalBorderWidth, borderRadius: 8, backgroundColor: currentTheme.backgroundSecondary, borderWidth: currentTheme.generalBorderWidth, borderColor: currentTheme.generalBorderColor}}>
-                                    {Object.keys(themes).map(name => <Pressable style={{padding: 10 - currentTheme.generalBorderWidth, backgroundColor: currentTheme.backgroundPrimary, borderRadius: 8, marginBottom: 5, borderColor: currentTheme.buttonBorderColor, borderWidth: currentTheme.buttonBorderWidth}} onPress={() => {currentTheme = themes[name]; currentThemeName = name; refreshStyles(); this.setState({rerender: this.state.rerender + 1})}}><Text>{name}{currentThemeName == name ? " (active)" : ""}</Text></Pressable>)}
+                                    {Object.keys(themes).map(name => <Pressable style={{padding: 10 - currentTheme.generalBorderWidth, backgroundColor: currentTheme.backgroundPrimary, borderRadius: 8, marginBottom: 5, borderColor: currentTheme.buttonBorderColor, borderWidth: currentTheme.buttonBorderWidth}} onPress={() => {
+                                        setTheme(name)
+                                        this.setState({rerender: this.state.rerender + 1})
+                                    }}><Text>{name}{currentThemeName == name ? " (active)" : ""}</Text></Pressable>)}
                                 </ScrollView>
                                 <Text>some text here</Text>
                             </ScrollView>
@@ -1086,287 +1089,6 @@ export default class App extends React.Component {
 
 
 
-
 // <themeing stuff>
-const themes = {
-    "Light": {
-        backgroundPrimary: '#ffffff',
-        backgroundSecondary: '#d8d8d8',
-        blockQuoteBackground: '#11111166',
-        textPrimary: '#000000',
-        textSecondary: '#555555',
-        accentColor: '#913edc',
-        accentColorForeground: '#000000',
-        contentType: 'dark',
-        buttonBorderWidth: 0,
-        messageBoxBorderWidth: 0,
-        generalBorderWidth: 0,
-        buttonBorderColorActive: "#3333ff",
-        statusOnline: "#3abf7e",
-        statusIdle: "#f39f00",
-        statusBusy: "#f84848",
-        statusStreaming: "#977eff",
-        statusOffline: "#a5a5a5",
-        statusInvisible: "#a5a5a5"
-    },
-    "Dark": {
-        backgroundPrimary: '#151515',
-        backgroundSecondary: '#202020',
-        blockQuoteBackground: '#11111166',
-        textPrimary: '#dddddd',
-        textSecondary: '#888888',
-        accentColor: '#913edc',
-        accentColorForeground: '#000000',
-        contentType: 'light',
-        buttonBorderWidth: 0,
-        messageBoxBorderWidth: 0,
-        generalBorderWidth: 0,
-        buttonBorderColorActive: "#3333ff",
-        statusOnline: "#3abf7e",
-        statusIdle: "#f39f00",
-        statusBusy: "#f84848",
-        statusStreaming: "#977eff",
-        statusOffline: "#a5a5a5",
-        statusInvisible: "#a5a5a5"
-    },
-    "Solarized": {
-        backgroundPrimary: '#001a20',
-        backgroundSecondary: '#05252d',
-        blockQuoteBackground: '#11111166',
-        textPrimary: '#dddddd',
-        textSecondary: '#888888',
-        accentColor: '#913edc',
-        accentColorForeground: '#000000',
-        contentType: 'light',
-        buttonBorderWidth: 0,
-        messageBoxBorderWidth: 0,
-        generalBorderWidth: 0,
-        buttonBorderColorActive: "#3333ff",
-        statusOnline: "#3abf7e",
-        statusIdle: "#f39f00",
-        statusBusy: "#f84848",
-        statusStreaming: "#977eff",
-        statusOffline: "#a5a5a5",
-        statusInvisible: "#a5a5a5"
-    },
-    "AMOLED": {
-        backgroundPrimary: '#000000',
-        backgroundSecondary: '#000000',
-        blockQuoteBackground: '#111111',
-        textPrimary: '#dddddd',
-        textSecondary: '#888888',
-        accentColor: '#913edc',
-        accentColorForeground: '#000000',
-        contentType: 'light',
-        buttonBorderColor: "#ffffff99",
-        buttonBorderWidth: 1,
-        messageBoxBorderColor: "#ffffff99",
-        messageBoxBorderWidth: 1,
-        generalBorderColor: "#ffffff22",
-        generalBorderWidth: 1,
-        buttonBorderColorActive: "#3333ff",
-        statusOnline: "#3abf7e",
-        statusIdle: "#f39f00",
-        statusBusy: "#f84848",
-        statusStreaming: "#977eff",
-        statusOffline: "#a5a5a5",
-        statusInvisible: "#a5a5a5"
-    }
-}
-var currentTheme = themes["Dark"]
-var currentThemeName = "Dark"
 
-var styles;
-function refreshStyles() {
-    styles = StyleSheet.create({
-        outer: {
-            flex: 1,
-            backgroundColor: currentTheme.backgroundSecondary
-        },
-        app: {
-            flex: 1,
-            backgroundColor: currentTheme.backgroundPrimary
-        },
-        mainView: {
-            flex: 1,
-            backgroundColor: currentTheme.backgroundPrimary,
-            borderRadius: 8
-        },
-        loggingInScreen: {
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        leftView: {
-            flex: 1,
-            backgroundColor: currentTheme.backgroundSecondary,
-            flexDirection: "row",
-            justifyContent: 'flex-start'
-        },
-        rightView: {
-            flex: 1,
-            backgroundColor: currentTheme.backgroundSecondary
-        },
-        textDefault: {
-            color: currentTheme.textPrimary
-        },
-        message: {
-            width: "100%",
-            flex: 1,
-            flexDirection: "row",
-            marginBottom: 3
-        },
-        messageGrouped: {
-            paddingLeft: 35,
-            width: "100%"
-        },
-        messageInner: {
-            flex: 1,
-            paddingLeft: 10
-        },
-        messageAvatar: {
-            width: 35,
-            height: 35,
-            borderRadius: 100000
-        },
-        messageAvatarReply: {
-            width: 15,
-            height: 15,
-            borderRadius: 100000
-        },
-        messageUsernameReply: {
-            marginLeft: 3,
-            marginRight: 3
-        },
-        typingBar: {
-            height: 26,
-            paddingLeft: 6,
-            padding: 3,
-            backgroundColor: currentTheme.backgroundSecondary,
-            borderBottomColor: currentTheme.backgroundPrimary,
-            borderBottomWidth: 1
-        },
-        messageUsername: {
-            fontWeight: 'bold'
-        },
-        serverButton: {
-            borderRadius: 5000,
-            width: 48,
-            height: 48,
-            margin: 4,
-            backgroundColor: currentTheme.backgroundPrimary,
-            overflow: "hidden"
-        },
-        serverIcon: {
-            width: 48,
-            height: 48
-        },
-        serverList: {
-            width: 56,
-            flexShrink: 1
-        },
-        channelList: {
-            flexGrow: 1000,
-            flex: 1000
-        },
-        channelButton: {
-            padding: 8 - currentTheme.generalBorderWidth,
-            margin: 3,
-            marginRight: 5,
-            borderRadius: 8,
-            flexDirection: 'row',
-            backgroundColor: currentTheme.backgroundPrimary,
-            borderWidth: currentTheme.buttonBorderWidth,
-            borderColor: currentTheme.buttonBorderColor
-        },
-        button: {
-            padding: 5,
-            paddingLeft: 10,
-            paddingRight: 10,
-            borderRadius: 8,
-            backgroundColor: currentTheme.backgroundSecondary,
-            margin: 5
-        },
-        channelButtonSelected: {
-            borderColor: currentTheme.buttonBorderColorActive,
-            borderWidth: currentTheme.buttonBorderWidth > 0 ? currentTheme.buttonBorderWidth : 1
-        },
-        messagesView: {
-            padding: 10,
-            flex: 1
-        },
-        messageBoxInner: {
-            flexDirection: 'row'
-        },
-        messageBoxOuter: {
-            backgroundColor: currentTheme.backgroundSecondary,
-            margin: 5,
-            borderRadius: 8,
-            overflow: "hidden",
-            borderColor: currentTheme.messageBoxBorderColor,
-            borderWidth: currentTheme.messageBoxBorderWidth
-        },
-        sendButton: {
-            margin: 3,
-            marginLeft: 0,
-            borderRadius: 8,
-            backgroundColor: currentTheme.backgroundPrimary,
-            width: 50,
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
-        headerIcon: {
-            margin: 5,
-            marginRight: 10,
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
-        messageBox: {
-            color: currentTheme.textPrimary,
-            paddingLeft: 10,
-            padding: 6,
-            flex: 1
-        },
-        channelHeader: {
-            height: 50,
-            backgroundColor: currentTheme.backgroundSecondary,
-            alignItems: 'center',
-            paddingLeft: 20,
-            flexDirection: 'row'
-        },
-        messageContentReply: {
-            height: 20,
-            marginLeft: 4
-        },
-        actionTile: {
-            height: 40,
-            width: "100%",
-            alignItems: 'center',
-            flexDirection: 'row',
-            backgroundColor: currentTheme.backgroundPrimary,
-            marginBottom: 3,
-            borderRadius: 8,
-            paddingLeft: 10,
-            paddingRight: 10,
-            margin: 3
-        },
-        replyingMessagePreview: {
-            padding: 4,
-            borderBottomColor: currentTheme.backgroundPrimary,
-            borderBottomWidth: 1,
-            flexDirection: 'row'
-        },
-        repliedMessagePreviews: {
-            paddingTop: 4
-        },
-        timestamp: {
-            fontSize: 12,
-            color: currentTheme.textSecondary,
-            position: 'relative',
-            top: 2,
-            left: 2
-        }
-    });
-}
-refreshStyles()
 // </themeing stuff>
---
+++ src/Theme.js
@@ -0,0 +1,287 @@
+import { StyleSheet } from 'react-native';
+export const themes = {
+    "Light": {
+        backgroundPrimary: '#ffffff',
+        backgroundSecondary: '#d8d8d8',
+        blockQuoteBackground: '#11111166',
+        textPrimary: '#000000',
+        textSecondary: '#555555',
+        accentColor: '#913edc',
+        accentColorForeground: '#000000',
+        contentType: 'dark',
+        buttonBorderWidth: 0,
+        messageBoxBorderWidth: 0,
+        generalBorderWidth: 0,
+        buttonBorderColorActive: "#3333ff",
+        statusOnline: "#3abf7e",
+        statusIdle: "#f39f00",
+        statusBusy: "#f84848",
+        statusStreaming: "#977eff",
+        statusOffline: "#a5a5a5",
+        statusInvisible: "#a5a5a5"
+    },
+    "Dark": {
+        backgroundPrimary: '#151515',
+        backgroundSecondary: '#202020',
+        blockQuoteBackground: '#11111166',
+        textPrimary: '#dddddd',
+        textSecondary: '#888888',
+        accentColor: '#913edc',
+        accentColorForeground: '#000000',
+        contentType: 'light',
+        buttonBorderWidth: 0,
+        messageBoxBorderWidth: 0,
+        generalBorderWidth: 0,
+        buttonBorderColorActive: "#3333ff",
+        statusOnline: "#3abf7e",
+        statusIdle: "#f39f00",
+        statusBusy: "#f84848",
+        statusStreaming: "#977eff",
+        statusOffline: "#a5a5a5",
+        statusInvisible: "#a5a5a5"
+    },
+    "Solarized": {
+        backgroundPrimary: '#001a20',
+        backgroundSecondary: '#05252d',
+        blockQuoteBackground: '#11111166',
+        textPrimary: '#dddddd',
+        textSecondary: '#888888',
+        accentColor: '#913edc',
+        accentColorForeground: '#000000',
+        contentType: 'light',
+        buttonBorderWidth: 0,
+        messageBoxBorderWidth: 0,
+        generalBorderWidth: 0,
+        buttonBorderColorActive: "#3333ff",
+        statusOnline: "#3abf7e",
+        statusIdle: "#f39f00",
+        statusBusy: "#f84848",
+        statusStreaming: "#977eff",
+        statusOffline: "#a5a5a5",
+        statusInvisible: "#a5a5a5"
+    },
+    "AMOLED": {
+        backgroundPrimary: '#000000',
+        backgroundSecondary: '#000000',
+        blockQuoteBackground: '#111111',
+        textPrimary: '#dddddd',
+        textSecondary: '#888888',
+        accentColor: '#913edc',
+        accentColorForeground: '#000000',
+        contentType: 'light',
+        buttonBorderColor: "#ffffff99",
+        buttonBorderWidth: 1,
+        messageBoxBorderColor: "#ffffff99",
+        messageBoxBorderWidth: 1,
+        generalBorderColor: "#ffffff22",
+        generalBorderWidth: 1,
+        buttonBorderColorActive: "#3333ff",
+        statusOnline: "#3abf7e",
+        statusIdle: "#f39f00",
+        statusBusy: "#f84848",
+        statusStreaming: "#977eff",
+        statusOffline: "#a5a5a5",
+        statusInvisible: "#a5a5a5"
+    }
+}
+export var currentTheme = themes["Dark"]
+export var currentThemeName = "Dark"
+
+export var styles;
+function refreshStyles() {
+    styles = StyleSheet.create({
+        outer: {
+            flex: 1,
+            backgroundColor: currentTheme.backgroundSecondary
+        },
+        app: {
+            flex: 1,
+            backgroundColor: currentTheme.backgroundPrimary
+        },
+        mainView: {
+            flex: 1,
+            backgroundColor: currentTheme.backgroundPrimary,
+            borderRadius: 8
+        },
+        loggingInScreen: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+        leftView: {
+            flex: 1,
+            backgroundColor: currentTheme.backgroundSecondary,
+            flexDirection: "row",
+            justifyContent: 'flex-start'
+        },
+        rightView: {
+            flex: 1,
+            backgroundColor: currentTheme.backgroundSecondary
+        },
+        textDefault: {
+            color: currentTheme.textPrimary
+        },
+        message: {
+            width: "100%",
+            flex: 1,
+            flexDirection: "row",
+            marginBottom: 3
+        },
+        messageGrouped: {
+            paddingLeft: 35,
+            width: "100%"
+        },
+        messageInner: {
+            flex: 1,
+            paddingLeft: 10
+        },
+        messageAvatar: {
+            width: 35,
+            height: 35,
+            borderRadius: 100000
+        },
+        messageAvatarReply: {
+            width: 15,
+            height: 15,
+            borderRadius: 100000
+        },
+        messageUsernameReply: {
+            marginLeft: 3,
+            marginRight: 3
+        },
+        typingBar: {
+            height: 26,
+            paddingLeft: 6,
+            padding: 3,
+            backgroundColor: currentTheme.backgroundSecondary,
+            borderBottomColor: currentTheme.backgroundPrimary,
+            borderBottomWidth: 1
+        },
+        messageUsername: {
+            fontWeight: 'bold'
+        },
+        serverButton: {
+            borderRadius: 5000,
+            width: 48,
+            height: 48,
+            margin: 4,
+            backgroundColor: currentTheme.backgroundPrimary,
+            overflow: "hidden"
+        },
+        serverIcon: {
+            width: 48,
+            height: 48
+        },
+        serverList: {
+            width: 56,
+            flexShrink: 1
+        },
+        channelList: {
+            flexGrow: 1000,
+            flex: 1000
+        },
+        channelButton: {
+            padding: 8 - currentTheme.generalBorderWidth,
+            margin: 3,
+            marginRight: 5,
+            borderRadius: 8,
+            flexDirection: 'row',
+            backgroundColor: currentTheme.backgroundPrimary,
+            borderWidth: currentTheme.buttonBorderWidth,
+            borderColor: currentTheme.buttonBorderColor
+        },
+        button: {
+            padding: 5,
+            paddingLeft: 10,
+            paddingRight: 10,
+            borderRadius: 8,
+            backgroundColor: currentTheme.backgroundSecondary,
+            margin: 5
+        },
+        channelButtonSelected: {
+            borderColor: currentTheme.buttonBorderColorActive,
+            borderWidth: currentTheme.buttonBorderWidth > 0 ? currentTheme.buttonBorderWidth : 1
+        },
+        messagesView: {
+            padding: 10,
+            flex: 1
+        },
+        messageBoxInner: {
+            flexDirection: 'row'
+        },
+        messageBoxOuter: {
+            backgroundColor: currentTheme.backgroundSecondary,
+            margin: 5,
+            borderRadius: 8,
+            overflow: "hidden",
+            borderColor: currentTheme.messageBoxBorderColor,
+            borderWidth: currentTheme.messageBoxBorderWidth
+        },
+        sendButton: {
+            margin: 3,
+            marginLeft: 0,
+            borderRadius: 8,
+            backgroundColor: currentTheme.backgroundPrimary,
+            width: 50,
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        headerIcon: {
+            margin: 5,
+            marginRight: 10,
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        messageBox: {
+            color: currentTheme.textPrimary,
+            paddingLeft: 10,
+            padding: 6,
+            flex: 1
+        },
+        channelHeader: {
+            height: 50,
+            backgroundColor: currentTheme.backgroundSecondary,
+            alignItems: 'center',
+            paddingLeft: 20,
+            flexDirection: 'row'
+        },
+        messageContentReply: {
+            height: 20,
+            marginLeft: 4
+        },
+        actionTile: {
+            height: 40,
+            width: "100%",
+            alignItems: 'center',
+            flexDirection: 'row',
+            backgroundColor: currentTheme.backgroundPrimary,
+            marginBottom: 3,
+            borderRadius: 8,
+            paddingLeft: 10,
+            paddingRight: 10,
+            margin: 3
+        },
+        replyingMessagePreview: {
+            padding: 4,
+            borderBottomColor: currentTheme.backgroundPrimary,
+            borderBottomWidth: 1,
+            flexDirection: 'row'
+        },
+        repliedMessagePreviews: {
+            paddingTop: 4
+        },
+        timestamp: {
+            fontSize: 12,
+            color: currentTheme.textSecondary,
+            position: 'relative',
+            top: 2,
+            left: 2
+        }
+    });
+}
+export function setTheme(themeName) {
+    currentThemeName = themeName
+    currentTheme = themes[themeName]
+    refreshStyles()
+}
+setTheme('Dark')
