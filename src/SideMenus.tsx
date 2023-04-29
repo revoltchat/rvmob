d27748a85e1e2d9a269971b5b0d1e601e023d7da
Rexogamer <beartechtalks@gmail.com>
2023-04-30 00:14:15 +0100

    refactor: convert LeftMenu to functional component

--- src/SideMenus.tsx
+++ src/SideMenus.tsx
@@ -4,7 +4,7 @@ import {View, TouchableOpacity, ScrollView, FlatList} from 'react-native';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
-import {User} from 'revolt.js';
+import {Server, User} from 'revolt.js';
 
 import {app, setFunction, client} from './Generic';
 import {MiniProfile, Avatar} from './Profile';
@@ -14,109 +14,108 @@ import {MarkdownView} from './components/common/MarkdownView';
 import {ChannelList} from './components/navigation/ChannelList';
 import {ServerList} from './components/navigation/ServerList';
 
-export class LeftMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentServer: null,
-    };
-    setFunction('openServer', s => {
-      this.setState({currentServer: s});
-    });
-  }
-  render() {
-    return (
-      <>
-        <View style={styles.leftView}>
-          <ScrollView key={'server-list'} style={styles.serverList}>
-            <TouchableOpacity
-              onPress={() => {
-                this.state.currentServer
-                  ? this.setState({
-                      currentServer: null,
-                      contextMenuUserServer: null,
-                    })
-                  : app.openStatusMenu(true);
-              }}
-              onLongPress={() => {
-                app.openProfile(client.user);
-              }}
-              delayLongPress={750}
-              key={client.user?._id}
-              style={{margin: 4}}>
-              <Avatar
-                key={`${client.user?._id}-avatar`}
-                user={client.user}
-                size={48}
-                backgroundColor={currentTheme.backgroundSecondary}
-                status
-              />
-            </TouchableOpacity>
-            <View
-              style={{
-                margin: 6,
-                height: 2,
-                width: '80%',
-                backgroundColor: currentTheme.backgroundPrimary,
-              }}
+export const LeftMenu = ({
+  currentChannel,
+  onChannelClick,
+  onLogOut,
+}: {
+  currentChannel: any;
+  onChannelClick: Function;
+  onLogOut: Function;
+}) => {
+  const [currentServer, setCurrentServer] = React.useState(
+    null as Server | null,
+  );
+  setFunction('openServer', (s: Server | null) => {
+    setCurrentServer(s);
+  });
+  return (
+    <>
+      <View style={styles.leftView}>
+        <ScrollView key={'server-list'} style={styles.serverList}>
+          <TouchableOpacity
+            onPress={() => {
+              currentServer ? setCurrentServer(null) : app.openStatusMenu(true);
+            }}
+            onLongPress={() => {
+              app.openProfile(client.user);
+            }}
+            delayLongPress={750}
+            key={client.user?._id}
+            style={{margin: 4}}>
+            <Avatar
+              key={`${client.user?._id}-avatar`}
+              user={client.user}
+              size={48}
+              backgroundColor={currentTheme.backgroundSecondary}
+              status
             />
-            <ServerList
-              onServerPress={(s: Server) => this.setState({currentServer: s})}
-              onServerLongPress={(s: Server) => app.openServerContextMenu(s)}
-            />
-          </ScrollView>
-          <ScrollView key={'channel-list'} style={styles.channelList}>
-            <ChannelList
-              onChannelClick={this.props.onChannelClick}
-              currentChannel={this.props.currentChannel}
-              currentServer={this.state.currentServer}
-            />
-          </ScrollView>
-        </View>
-        <View
-          style={{
-            height: 50,
-            width: '100%',
-            backgroundColor: currentTheme.backgroundSecondary,
-            borderTopWidth: currentTheme.generalBorderWidth,
-            borderColor: currentTheme.generalBorderColor,
-            flexDirection: 'row',
-          }}>
-          <Button
-            key={'bottom-nav-friends'}
-            onPress={() => this.props.onChannelClick('friends')}
-            backgroundColor={currentTheme.backgroundPrimary}>
-            <MaterialIcon
-              name="group"
-              size={20}
-              color={currentTheme.foregroundPrimary}
-            />
-          </Button>
-          <Button
-            key={'bottom-nav-settings'}
-            onPress={() => app.openSettings(true)}
-            backgroundColor={currentTheme.backgroundPrimary}>
-            <FAIcon
-              name="gear"
-              size={20}
-              color={currentTheme.foregroundPrimary}
-            />
-          </Button>
-          <Button
-            key={'bottom-nav-logout'}
-            onPress={this.props.onLogOut}
-            backgroundColor={currentTheme.backgroundPrimary}>
-            <MaterialIcon
-              name="logout"
-              size={20}
-              color={currentTheme.foregroundPrimary}
-            />
-          </Button>
-        </View>
-      </>
-    );
-  }
-}
+          </TouchableOpacity>
+          <View
+            style={{
+              margin: 6,
+              height: 2,
+              width: '80%',
+              backgroundColor: currentTheme.backgroundPrimary,
+            }}
+          />
+          <ServerList
+            onServerPress={(s: Server) => setCurrentServer(s)}
+            onServerLongPress={(s: Server) => app.openServerContextMenu(s)}
+          />
+        </ScrollView>
+        <ScrollView key={'channel-list'} style={styles.channelList}>
+          <ChannelList
+            onChannelClick={onChannelClick}
+            currentChannel={currentChannel}
+            currentServer={currentServer}
+          />
+        </ScrollView>
+      </View>
+      <View
+        style={{
+          height: 50,
+          width: '100%',
+          backgroundColor: currentTheme.backgroundSecondary,
+          borderTopWidth: currentTheme.generalBorderWidth,
+          borderColor: currentTheme.generalBorderColor,
+          flexDirection: 'row',
+        }}>
+        <Button
+          key={'bottom-nav-friends'}
+          onPress={() => onChannelClick('friends')}
+          backgroundColor={currentTheme.backgroundPrimary}>
+          <MaterialIcon
+            name="group"
+            size={20}
+            color={currentTheme.foregroundPrimary}
+          />
+        </Button>
+        <Button
+          key={'bottom-nav-settings'}
+          onPress={() => app.openSettings(true)}
+          backgroundColor={currentTheme.backgroundPrimary}>
+          <FAIcon
+            name="gear"
+            size={20}
+            color={currentTheme.foregroundPrimary}
+          />
+        </Button>
+        <Button
+          key={'bottom-nav-logout'}
+          onPress={onLogOut}
+          backgroundColor={currentTheme.backgroundPrimary}>
+          <MaterialIcon
+            name="logout"
+            size={20}
+            color={currentTheme.foregroundPrimary}
+          />
+        </Button>
+      </View>
+    </>
+  );
+};
+
 export class RightMenu extends React.Component {
   constructor(props) {
     super(props);
