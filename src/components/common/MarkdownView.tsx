62617ef0f9a79a1794bceb4127a458944ec4afc7
New Creative <ianparra200@gmail.com>
2023-08-03 21:45:41 -0400

    emoji packs render
    + solved crash when emoji ID is not found

--- src/components/common/MarkdownView.tsx
+++ src/components/common/MarkdownView.tsx
@@ -5,13 +5,12 @@ import Markdown, {hasParents, MarkdownIt} from 'react-native-markdown-display';
 import {SvgUri} from 'react-native-svg';
 import FastImage from 'react-native-fast-image';
 import {
-  emojiDictionary,
   RE_UNICODE_EMOJI,
   RE_DEFAULT_EMOJI,
   RE_CUSTOM_EMOJI,
-  parseEmoji,
 } from '../../lib/emojis';
-import {openUrl, client} from '../../Generic';
+import {unicodeEmojiURL, RevoltEmojiDictionary} from 'revkit'
+import {openUrl, client, app} from '../../Generic';
 import {currentTheme, styles} from '../../Theme';
 import {Text} from './atoms';
 
@@ -39,28 +38,28 @@ const Spoiler = ({content}) => {
     </SpoilerContext.Provider>
   );
 };
-const SvgEmoji = ({id}) => {
+const SvgEmoji = ({id, pack}) => {
   const [fail, setFail] = React.useState(false);
-  if (fail) return <span>{`:${id}:`}</span>;
-  if (Object.hasOwn(emojiDictionary, id)) id = emojiDictionary[id];
+  if (fail) return <Text>{`:${id}:`}</Text>;
+  if (Object.hasOwn(RevoltEmojiDictionary, id)) id = RevoltEmojiDictionary[id];
   return (
     <SvgUri
       width={styles.emoji.width}
       height={styles.emoji.height}
       style={styles.emoji}
-      uri={parseEmoji(id)}
+      uri={unicodeEmojiURL(id, pack)}
       onError={() => setFail(true)}
-      fallback={<span>`:${id}:`</span>}
     />
   );
 };
 const CustomEmoji = ({id}) => {
   const [fail, setFail] = React.useState(false);
-  if (fail) return <span>{`:${id}:`}</span>;
+  if (fail) return <Text>{`:${id}:`}</Text>;
   return (
     <FastImage
       style={styles.emoji}
       source={{uri: `${client.configuration.features.autumn.url}/emojis/${id}`}}
+      onError={()=>setFail(true)}
     />
   );
 };
@@ -128,33 +127,36 @@ const spoilerRule = {
     );
   },
 };
+
 /* Can this function be more efficient? */
 function renderEmojis(content: string): Array<SvgEmoji | CustomEmoji | Text> {
   const tokens = content.split(RE_CUSTOM_EMOJI);
+  const emojiPack = app.settings.get('ui.messaging.emojiPack')
   const elements = tokens.flatMap((part, index) => {
     if (index % 2 == 1) return <CustomEmoji key={index} id={part} />;
-    let subparts = part
-      .split(RE_DEFAULT_EMOJI)
-      .map((id, i) =>
-        i % 2 == 1 ? <SvgEmoji key={`default-emoji-${i}`} id={id} /> : id,
-      )
-      .filter(t => t);
-    subparts = subparts.flatMap(s => {
-      if (typeof s != 'string') return s;
-      let emojis = s.match(RE_UNICODE_EMOJI);
-      if (emojis) {
-        let text = s.split(RE_UNICODE_EMOJI);
-        emojis = emojis.map((u, i) => (
-          <SvgEmoji key={`unicode-emoji-${i}`} id={u} />
-        ));
-        for (let i = 0; i < text.length; i++) {
-          if (text[i])
-            emojis.splice(2 * i, 0, <Text key={`text-${i}`}>{text[i]}</Text>);
-        }
-        return emojis;
-      }
-      return s;
-    });
+    let subparts = part.split(RE_DEFAULT_EMOJI)
+    if(emojiPack != 'system'){
+        subparts = subparts.map((id, i) =>
+        i % 2 == 1 ? <SvgEmoji key={`default-emoji-${i}`} id={id} pack={emojiPack} /> : id,
+        ).filter(t => t).flatMap(s => {
+              if (typeof s != 'string') return s;
+              let emojis = s.match(RE_UNICODE_EMOJI);
+              if (emojis) {
+                let text = s.split(RE_UNICODE_EMOJI);
+                emojis = emojis.map((u, i) => (
+                  <SvgEmoji key={`unicode-emoji-${i}`} id={u} pack={emojiPack} />
+                ));
+                for (let i = 0; i < text.length; i++) {
+                  if (text[i])
+                    emojis.splice(2 * i, 0, text[i]);
+                }
+                return emojis;
+              }
+              return s;
+            });
+    }else{
+        subparts = subparts.map((id, i)=> i%2==1 ? (RevoltEmojiDictionary[id]??`:${id}:`) : id)
+    }
     return subparts;
   });
   return elements.length > 1 ? elements : elements[0];
