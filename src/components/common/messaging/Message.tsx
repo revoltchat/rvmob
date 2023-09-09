65e5b6cdff5330929914826943baf8f1c7155abc
Rexogamer <beartechtalks@gmail.com>
2023-09-09 03:42:33 +0100

    fix: consistent spacing for messages with/without mentions

--- src/components/common/messaging/Message.tsx
+++ src/components/common/messaging/Message.tsx
@@ -280,10 +280,6 @@ export const Message = observer((props: MessageProps) => {
                 ...(props.grouped ? styles.messageGrouped : styles.message),
                 ...(mentionsUser
                   ? {
-                      paddingStart: props.grouped ? 37 : 2,
-                      borderRadius: 4,
-                      borderLeftWidth: 3,
-                      borderStyle: 'solid',
                       borderColor: currentTheme.mentionBorder,
                       backgroundColor: currentTheme.mentionBackground,
                     }
