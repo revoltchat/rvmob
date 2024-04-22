9b111f4d47451294dfe928395071e8cffdc71322
Rexogamer <beartechtalks@gmail.com>
2024-04-22 22:39:51 +0100

    fix: move expect-error comment

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -6,10 +6,10 @@ import {resources} from '@rvmob-i18n/languages';
 import {languageDetectorPlugin} from '@rvmob-i18n/getLanguage';
 import {Text} from '@rvmob/components/common/atoms';
 
-// @ts-expect-error upstream typing issue (https://github.com/i18next/react-i18next/issues/1648)
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(languageDetectorPlugin) // get the user's language if it's stored
+  // @ts-expect-error upstream typing issue (https://github.com/i18next/react-i18next/issues/1648)
   .init({
     fallbackLng: 'en',
 
