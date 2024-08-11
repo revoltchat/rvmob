8dd2243c345615b23c9d1c917debcdb46ee2ed88
Rexogamer <beartechtalks@gmail.com>
2024-08-11 18:34:15 +0100

    refactor: split condition

--- App.tsx
+++ App.tsx
@@ -343,204 +343,196 @@ class MainView extends ReactComponent {
               />
             </View>
           </>
-        ) : this.state.status === 'awaitingLogin' ||
-          this.state.status === 'tryingLogin' ? (
+        ) : this.state.status === 'awaitingLogin' ? (
           <>
-            {this.state.status === 'awaitingLogin' ? (
-              <>
-                <View
-                  style={{
-                    marginTop: 12,
-                    marginStart: 8,
-                    marginEnd: 4,
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    flexDirection: 'row',
-                  }}>
-                  {this.state.loginType !== '' ? (
-                    <BackButton
-                      callback={() => {
-                        this.setState({loginType: ''});
-                      }}
-                    />
-                  ) : (
-                    <View />
-                  )}
-                  <TouchableOpacity
-                    onPress={() => this.setState({status: 'loginSettings'})}>
-                    <MaterialIcon
-                      name="more-vert"
-                      size={30}
-                      color={currentTheme.foregroundPrimary}
-                    />
-                  </TouchableOpacity>
-                </View>
-                <View
-                  style={{
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    flex: 1,
-                  }}>
+            <View
+              style={{
+                marginTop: 12,
+                marginStart: 8,
+                marginEnd: 4,
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                flexDirection: 'row',
+              }}>
+              {this.state.loginType !== '' ? (
+                <BackButton
+                  callback={() => {
+                    this.setState({loginType: ''});
+                  }}
+                />
+              ) : (
+                <View />
+              )}
+              <TouchableOpacity
+                onPress={() => this.setState({status: 'loginSettings'})}>
+                <MaterialIcon
+                  name="more-vert"
+                  size={30}
+                  color={currentTheme.foregroundPrimary}
+                />
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                flex: 1,
+              }}>
+              <Text
+                style={{
+                  fontFamily: 'Inter',
+                  fontWeight: 'bold',
+                  fontSize: 48,
+                }}>
+                RVMob
+              </Text>
+              {this.state.loginType === 'email' ? (
+                <>
+                  <TextInput
+                    placeholderTextColor={currentTheme.foregroundSecondary}
+                    style={styles.loginInput}
+                    placeholder={t('app.login.forms.email_placeholder')}
+                    keyboardType={'email-address'}
+                    autoComplete={'email'}
+                    onChangeText={(text: string) => {
+                      this.setState({emailInput: text});
+                    }}
+                    value={this.state.emailInput}
+                  />
+                  <TextInput
+                    placeholderTextColor={currentTheme.foregroundSecondary}
+                    style={styles.loginInput}
+                    secureTextEntry={true}
+                    autoComplete={'password'}
+                    placeholder={t('app.login.forms.password_placeholder')}
+                    onChangeText={text => {
+                      this.setState({passwordInput: text});
+                    }}
+                    value={this.state.passwordInput}
+                  />
+                  {this.state.askForTFACode === true ? (
+                    <>
+                      <TextInput
+                        placeholderTextColor={currentTheme.foregroundSecondary}
+                        style={styles.loginInput}
+                        placeholder={t('app.login.forms.mfa_placeholder')}
+                        onChangeText={text => {
+                          this.setState({tfaInput: text});
+                        }}
+                        value={this.state.tfaInput}
+                      />
+                    </>
+                  ) : null}
+                  <Button onPress={async () => await loginRegular(this)}>
+                    <Text font={'Inter'}>Log in</Text>
+                  </Button>
+                  {this.state.logInError ? (
+                    <Text>
+                      {this.state.logInError.message ||
+                        this.state.logInError.toString()}
+                    </Text>
+                  ) : null}
+                </>
+              ) : this.state.loginType === 'token' ? (
+                <>
+                  <TextInput
+                    placeholderTextColor={currentTheme.foregroundSecondary}
+                    style={styles.loginInput}
+                    placeholder={t('app.login.forms.session_token_placeholder')}
+                    onChangeText={text => {
+                      this.setState({tokenInput: text});
+                    }}
+                    value={this.state.tokenInput}
+                  />
+                  <Link
+                    link={
+                      'https://infi.sh/posts/revolt-tokens?utm_source=rvmob'
+                    }
+                    label={t('app.login.token_info_label')}
+                    style={{fontFamily: 'Inter', fontWeight: 'bold'}}
+                  />
+                  <Button onPress={async () => await loginWithToken(this)}>
+                    <Text font={'Inter'}>Log in</Text>
+                  </Button>
+                  {this.state.logInError ? (
+                    <Text>
+                      {this.state.logInError.message ?? this.state.logInError}
+                    </Text>
+                  ) : null}
+                </>
+              ) : (
+                <>
                   <Text
+                    font={'Inter'}
                     style={{
-                      fontFamily: 'Inter',
+                      marginVertical: 8,
+                      fontSize: 18,
                       fontWeight: 'bold',
-                      fontSize: 48,
                     }}>
-                    RVMob
+                    {t('app.login.subheader')}
                   </Text>
-                  {this.state.loginType === 'email' ? (
-                    <>
-                      <TextInput
-                        placeholderTextColor={currentTheme.foregroundSecondary}
-                        style={styles.loginInput}
-                        placeholder={t('app.login.forms.email_placeholder')}
-                        keyboardType={'email-address'}
-                        autoComplete={'email'}
-                        onChangeText={(text: string) => {
-                          this.setState({emailInput: text});
-                        }}
-                        value={this.state.emailInput}
-                      />
-                      <TextInput
-                        placeholderTextColor={currentTheme.foregroundSecondary}
-                        style={styles.loginInput}
-                        secureTextEntry={true}
-                        autoComplete={'password'}
-                        placeholder={t('app.login.forms.password_placeholder')}
-                        onChangeText={text => {
-                          this.setState({passwordInput: text});
-                        }}
-                        value={this.state.passwordInput}
-                      />
-                      {this.state.askForTFACode === true ? (
-                        <>
-                          <TextInput
-                            placeholderTextColor={
-                              currentTheme.foregroundSecondary
-                            }
-                            style={styles.loginInput}
-                            placeholder={t('app.login.forms.mfa_placeholder')}
-                            onChangeText={text => {
-                              this.setState({tfaInput: text});
-                            }}
-                            value={this.state.tfaInput}
-                          />
-                        </>
-                      ) : null}
-                      <Button onPress={async () => await loginRegular(this)}>
-                        <Text font={'Inter'}>Log in</Text>
-                      </Button>
-                      {this.state.logInError ? (
-                        <Text>
-                          {this.state.logInError.message ||
-                            this.state.logInError.toString()}
-                        </Text>
-                      ) : null}
-                    </>
-                  ) : this.state.loginType === 'token' ? (
-                    <>
-                      <TextInput
-                        placeholderTextColor={currentTheme.foregroundSecondary}
-                        style={styles.loginInput}
-                        placeholder={t(
-                          'app.login.forms.session_token_placeholder',
-                        )}
-                        onChangeText={text => {
-                          this.setState({tokenInput: text});
-                        }}
-                        value={this.state.tokenInput}
-                      />
-                      <Link
-                        link={
-                          'https://infi.sh/posts/revolt-tokens?utm_source=rvmob'
-                        }
-                        label={t('app.login.token_info_label')}
-                        style={{fontFamily: 'Inter', fontWeight: 'bold'}}
-                      />
-                      <Button onPress={async () => await loginWithToken(this)}>
-                        <Text font={'Inter'}>Log in</Text>
-                      </Button>
-                      {this.state.logInError ? (
-                        <Text>
-                          {this.state.logInError.message ??
-                            this.state.logInError}
-                        </Text>
-                      ) : null}
-                    </>
-                  ) : (
-                    <>
+                  <Button
+                    onPress={() => {
+                      this.setState({loginType: 'email'});
+                    }}
+                    style={{alignItems: 'center', width: '80%'}}>
+                    <Text
+                      font={'Inter'}
+                      style={{fontSize: 16, fontWeight: 'bold'}}>
+                      {t('app.login.options.login_regular')}
+                    </Text>
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      this.setState({loginType: 'token'});
+                    }}
+                    style={{alignItems: 'center', width: '80%'}}>
+                    <Text
+                      font={'Inter'}
+                      style={{fontSize: 16, fontWeight: 'bold'}}>
+                      {t('app.login.options.login_session_token')}
+                    </Text>
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      openUrl(
+                        client.configuration
+                          ? `${client.configuration.app}/login/create`
+                          : OFFICIAL_INSTANCE_SIGNUP_URL,
+                      );
+                    }}
+                    style={{width: '80%'}}>
+                    <View style={{alignItems: 'center'}}>
                       <Text
                         font={'Inter'}
-                        style={{
-                          marginVertical: 8,
-                          fontSize: 18,
-                          fontWeight: 'bold',
-                        }}>
-                        {t('app.login.subheader')}
+                        style={{fontSize: 16, fontWeight: 'bold'}}>
+                        {t('app.login.options.signup')}
                       </Text>
-                      <Button
-                        onPress={() => {
-                          this.setState({loginType: 'email'});
-                        }}
-                        style={{alignItems: 'center', width: '80%'}}>
-                        <Text
-                          font={'Inter'}
-                          style={{fontSize: 16, fontWeight: 'bold'}}>
-                          {t('app.login.options.login_regular')}
-                        </Text>
-                      </Button>
-                      <Button
-                        onPress={() => {
-                          this.setState({loginType: 'token'});
-                        }}
-                        style={{alignItems: 'center', width: '80%'}}>
-                        <Text
-                          font={'Inter'}
-                          style={{fontSize: 16, fontWeight: 'bold'}}>
-                          {t('app.login.options.login_session_token')}
-                        </Text>
-                      </Button>
-                      <Button
-                        onPress={() => {
-                          openUrl(
-                            client.configuration
-                              ? `${client.configuration.app}/login/create`
-                              : OFFICIAL_INSTANCE_SIGNUP_URL,
-                          );
-                        }}
-                        style={{width: '80%'}}>
-                        <View style={{alignItems: 'center'}}>
-                          <Text
-                            font={'Inter'}
-                            style={{fontSize: 16, fontWeight: 'bold'}}>
-                            {t('app.login.options.signup')}
-                          </Text>
-                          <Text font={'Inter'}>
-                            {t('app.login.options.signup_body')}
-                          </Text>
-                        </View>
-                      </Button>
-                      <Text
-                        font={'Inter'}
-                        colour={currentTheme.foregroundSecondary}>
-                        {t('app.login.instance_notice', {
-                          url: app.settings.get('app.instance'),
-                        })}
+                      <Text font={'Inter'}>
+                        {t('app.login.options.signup_body')}
                       </Text>
-                    </>
-                  )}
-                </View>
-              </>
-            ) : (
-              <View style={styles.loggingInScreen}>
-                <Text style={{fontSize: 30, fontWeight: 'bold'}}>
-                  {this.state.loadingStage === 'connected' ? t('app.loading.generic') : t('app.loading.connecting')}
-                </Text>
-                <Text style={styles.remark}>{selectedRemark || null}</Text>
-              </View>
-            )}
+                    </View>
+                  </Button>
+                  <Text
+                    font={'Inter'}
+                    colour={currentTheme.foregroundSecondary}>
+                    {t('app.login.instance_notice', {
+                      url: app.settings.get('app.instance'),
+                    })}
+                  </Text>
+                </>
+              )}
+            </View>
           </>
+        ) : this.state.status === 'tryingLogin' ? (
+          <View style={styles.loggingInScreen}>
+            <Text style={{fontSize: 30, fontWeight: 'bold'}}>
+              {this.state.loadingStage === 'connected'
+                ? t('app.loading.generic')
+                : t('app.loading.connecting')}
+            </Text>
+            <Text style={styles.remark}>{selectedRemark || null}</Text>
+          </View>
         ) : this.state.status === 'loginSettings' ? (
           <LoginSettingsPage state={this} />
         ) : (
