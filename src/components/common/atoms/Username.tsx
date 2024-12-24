89aca4dc4e42850671c4b20388cd38ac30ff17a2
Rexogamer <beartechtalks@gmail.com>
2024-12-24 11:08:44 +0000

    refactor: split up Username into smaller components

--- src/components/common/atoms/Username.tsx
+++ src/components/common/atoms/Username.tsx
@@ -10,6 +10,20 @@ import {USER_IDS} from '@rvmob/lib/consts';
 import {ThemeContext} from '@rvmob/lib/themes';
 import {getColour} from '@rvmob/lib/utils';
 
+type UsernameCoreProps = {
+  user: User;
+  size: number;
+  name: string;
+  colour: string;
+  skipDisplayName?: boolean;
+};
+
+type UsernameBadgeProps = {
+  user: User;
+  size?: number;
+  masquerade?: string | null;
+};
+
 type UsernameProps = {
   server?: Server;
   user?: User | null;
@@ -19,6 +33,61 @@ type UsernameProps = {
   color?: string;
   skipDisplayName?: boolean;
 };
+
+const UsernameCore = observer(
+  ({user, size, name, colour, skipDisplayName}: UsernameCoreProps) => {
+    return (
+      <Text
+        colour={colour}
+        style={{
+          fontWeight: 'bold',
+          fontSize: size,
+        }}>
+        {skipDisplayName ? '@' : null}
+        {name}
+        {skipDisplayName ? `#${user!.discriminator}` : null}
+      </Text>
+    );
+  },
+);
+
+const UsernameBadge = observer(
+  ({user, size, masquerade}: UsernameBadgeProps) => {
+    const {currentTheme} = useContext(ThemeContext);
+
+    const badgeSize = (size || 14) * 0.6;
+
+    const bridgedMessage =
+      user._id === USER_IDS.automod && masquerade !== undefined;
+
+    const badgeStyle = {
+      color: currentTheme.accentColorForeground,
+      backgroundColor: currentTheme.accentColor,
+      marginInlineStart: badgeSize * 0.4,
+      paddingHorizontal: badgeSize * 0.4,
+      alignSelf: 'center' as const,
+      borderRadius: 2,
+      fontSize: badgeSize,
+      top: badgeSize * 0.1,
+    };
+
+    return (
+      <>
+        {bridgedMessage ? (
+          <Text style={badgeStyle}>BRIDGE</Text>
+        ) : (
+          <>
+            {user?.bot ? <Text style={badgeStyle}>BOT</Text> : null}
+            {masquerade ? <Text style={badgeStyle}>MASQ.</Text> : null}
+            {user?._id === USER_IDS.platformModeration ? (
+              <Text style={badgeStyle}>SYSTEM</Text>
+            ) : null}
+          </>
+        )}
+      </>
+    );
+  },
+);
 
 export const Username = observer(
   ({
@@ -37,73 +106,49 @@ export const Username = observer(
         <Text style={size ? {fontSize: size} : {}}>{'<Unknown User>'}</Text>
       );
     }
+
     const memberObject = server
       ? client.members.getKey({
           server: server?._id,
           user: user._id,
         })
       : undefined;
-    let roleColor = color
+
+    let roleColour = color
       ? getColour(color, currentTheme)
       : currentTheme.foregroundPrimary;
+
     const name =
       server && memberObject?.nickname
         ? memberObject?.nickname
         : !skipDisplayName
         ? user.display_name ?? user.username
         : user.username;
+
     if (server && memberObject?.roles && memberObject?.roles?.length > 0) {
       let srv = client.servers.get(memberObject._id.server);
       if (srv?.roles) {
-        roleColor = getColour(
+        roleColour = getColour(
           memberObject.roleColour ?? currentTheme.foregroundPrimary,
           currentTheme,
         );
       }
     }
-    let badgeSize = (size || 14) * 0.6;
-    let bridgedMessage =
-      user._id === USER_IDS.automod && masquerade !== undefined;
-    let badgeStyle = {
-      color: currentTheme.accentColorForeground,
-      backgroundColor: currentTheme.accentColor,
-      marginLeft: badgeSize * 0.3,
-      paddingLeft: badgeSize * 0.4,
-      paddingRight: badgeSize * 0.4,
-      borderRadius: 3,
-      fontSize: badgeSize,
-      height: badgeSize + badgeSize * 0.45,
-      top: badgeSize * 0.5,
-    };
+
+    const usernameSize =
+      size || (app.settings.get('ui.messaging.fontSize') as number) || 14;
+
     return (
       <View style={{flexDirection: 'row'}}>
-        <Text
-          colour={roleColor}
-          style={{
-            fontWeight: 'bold',
-            fontSize:
-              size ||
-              (app.settings.get('ui.messaging.fontSize') as number) ||
-              14,
-          }}>
-          {skipDisplayName ? '@' : null}
-          {masquerade ?? name}
-          {skipDisplayName ? `#${user.discriminator}` : null}
-        </Text>
+        <UsernameCore
+          user={user}
+          size={usernameSize}
+          colour={roleColour}
+          name={masquerade ?? name}
+          skipDisplayName={skipDisplayName}
+        />
         {!noBadge ? (
-          <>
-            {bridgedMessage ? (
-              <Text style={badgeStyle}>BRIDGE</Text>
-            ) : (
-              <>
-                {user?.bot ? <Text style={badgeStyle}>BOT</Text> : null}
-                {masquerade ? <Text style={badgeStyle}>MASQ.</Text> : null}
-                {user?._id === USER_IDS.platformModeration ? (
-                  <Text style={badgeStyle}>SYSTEM</Text>
-                ) : null}
-              </>
-            )}
-          </>
+          <UsernameBadge user={user} size={size} masquerade={masquerade} />
         ) : null}
       </View>
     );
