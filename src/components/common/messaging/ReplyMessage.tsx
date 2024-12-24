bf25d76d2e6dd622c7ac90cdcc60b676b1ecee69
Rexogamer <beartechtalks@gmail.com>
2024-12-24 11:35:22 +0000

    fix: render system messages in replies

--- src/components/common/messaging/ReplyMessage.tsx
+++ src/components/common/messaging/ReplyMessage.tsx
@@ -4,6 +4,7 @@ import {useTranslation} from 'react-i18next';
 import {Message} from 'revolt.js';
 
 import {Avatar, Text, Username} from '@rvmob/components/common/atoms';
+import {SystemMessage} from './SystemMessage';
 import {commonValues} from '@rvmob/lib/themes';
 
 type ReplyProps = {
@@ -16,48 +17,46 @@ type ReplyProps = {
 export const ReplyMessage = (props: ReplyProps) => {
   const {t} = useTranslation();
 
-  if (!props.message?.system) {
-    return (
-      <View style={{alignItems: 'center', flexDirection: 'row'}}>
-        {props.showSymbol ? (
-          <Text
-            style={{
-              fontWeight: 'bold',
-              marginHorizontal: props.symbolMargin ?? 21,
-            }}>
-            ↱
-          </Text>
-        ) : null}
-        {props.message ? (
-          props.message.author ? (
-            <>
-              <Avatar
-                user={props.message.author}
-                server={props.message.channel?.server}
-                masquerade={props.message.generateMasqAvatarURL()}
-                size={16}
-              />
-              <Text style={{marginLeft: 4}}>{props.mention ? '@' : ''}</Text>
-              <Username
-                user={props.message.author}
-                server={props.message.channel?.server}
-                masquerade={props.message.masquerade?.name}
-              />
-              <Text style={localStyles.messageContentReply}>
-                {props.message.content?.split('\n').join(' ')}
-              </Text>
-            </>
-          ) : null
-        ) : (
-          <Text style={localStyles.messageContentReply}>
-            {t('app.messaging.reply_not_loaded')}
-          </Text>
-        )}
-      </View>
-    );
-  } else {
-    return <></>;
-  }
+  return (
+    <View style={{alignItems: 'center', flexDirection: 'row'}}>
+      {props.showSymbol ? (
+        <Text
+          style={{
+            fontWeight: 'bold',
+            marginHorizontal: props.symbolMargin ?? 21,
+          }}>
+          ↱
+        </Text>
+      ) : null}
+      {props.message ? (
+        props.message.system ? (
+          <SystemMessage message={props.message} isReply />
+        ) : props.message.author ? (
+          <>
+            <Avatar
+              user={props.message.author}
+              server={props.message.channel?.server}
+              masquerade={props.message.generateMasqAvatarURL()}
+              size={16}
+            />
+            <Text style={{marginLeft: 4}}>{props.mention ? '@' : ''}</Text>
+            <Username
+              user={props.message.author}
+              server={props.message.channel?.server}
+              masquerade={props.message.masquerade?.name}
+            />
+            <Text style={localStyles.messageContentReply}>
+              {props.message.content?.split('\n').join(' ')}
+            </Text>
+          </>
+        ) : null
+      ) : (
+        <Text style={localStyles.messageContentReply}>
+          {t('app.messaging.reply_not_loaded')}
+        </Text>
+      )}
+    </View>
+  );
 };
 
 const localStyles = StyleSheet.create({
