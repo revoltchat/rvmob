61757626a2f4e501274d557ade82fada0d99baa0
Rexogamer <beartechtalks@gmail.com>
2025-01-14 01:28:31 +0000

    feat: copy role ID on press

--- src/components/common/profile/RoleView.tsx
+++ src/components/common/profile/RoleView.tsx
@@ -1,13 +1,16 @@
 import {useContext} from 'react';
-import {View} from 'react-native';
+import {Pressable, View} from 'react-native';
+import {useTranslation} from 'react-i18next';
 import {observer} from 'mobx-react-lite';
+
+import Clipboard from '@react-native-clipboard/clipboard';
 
 import type {Server, User} from 'revolt.js';
 
 import {client} from '@rvmob/lib/client';
 import {Text} from '@rvmob/components/common/atoms/Text';
 import {commonValues, ThemeContext} from '@rvmob/lib/themes';
-import {getColour} from '@rvmob/lib/utils';
+import {getColour, showToast} from '@rvmob/lib/utils';
 
 type RoleViewProps = {
   server: Server;
@@ -17,13 +20,30 @@ type RoleViewProps = {
 export const RoleView = observer(({server, user}: RoleViewProps) => {
   const {currentTheme} = useContext(ThemeContext);
 
-  let memberObject = client.members.getKey({
+  const {t} = useTranslation();
+
+  const handlePress = (id: string) => {
+    Clipboard.setString(id);
+    showToast(t('app.profile.roles.copied_role_id_toast'));
+  };
+
+  // don't bother doing anything if the server has no roles
+  if (!server.roles) {
+    return <></>;
+  }
+
+  const memberObject = client.members.getKey({
     server: server?._id,
     user: user?._id,
   });
 
-  let roles = memberObject?.roles?.map(r => server.roles![r]) || null;
-  return memberObject && roles ? (
+  if (!memberObject || !memberObject.roles) {
+    return <></>;
+  }
+
+  const roles = memberObject.roles.map(r => server.roles![r]);
+
+  return (
     <>
       <Text type={'profile'}>ROLES</Text>
       <View
@@ -33,8 +53,9 @@ export const RoleView = observer(({server, user}: RoleViewProps) => {
           flexWrap: 'wrap',
           rowGap: commonValues.sizes.small,
         }}>
-        {roles.map((r, i, a) => (
-          <View
+        {roles.map((r, i) => (
+          <Pressable
+            onPress={() => handlePress(memberObject.roles![i])}
             key={`roleview-${server._id}-${r.name}-${i}`}
             style={{
               flexDirection: 'row',
@@ -58,9 +79,9 @@ export const RoleView = observer(({server, user}: RoleViewProps) => {
               }}
             />
             <Text>{r.name}</Text>
-          </View>
+          </Pressable>
         ))}
       </View>
     </>
-  ) : null;
+  );
 });
