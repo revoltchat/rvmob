61d5ac8faaa6aa123f82ef2cc7bc5c7782f5ea53
Rexogamer <beartechtalks@gmail.com>
2025-01-19 09:45:41 +0000

    chore: clean up crossplat file
    RNFI is dead and I'm not intending to go back, so remove the commented out code

--- src/crossplat/Image.ts
+++ src/crossplat/Image.ts
@@ -1,7 +1,1 @@
-import {Image as CoreImage} from 'react-native';
-
-export const Image = CoreImage;
-
-//import FastImage from 'react-native-fast-image';
-
-//export const Image = FastImage;
+export {Image} from 'react-native';
