85da9fd2aaecee4a938f1a95104667c88de932ac
Rexogamer <beartechtalks@gmail.com>
2025-01-19 12:10:34 +0000

    chore: clean up unused code/dep

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-get-random-values'; // react native moment
 
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {ErrorBoundary} from 'react-error-boundary';
 
@@ -10,13 +10,10 @@ import {setFunction} from '@clerotri/Generic';
 import {MainView} from '@clerotri/MainView';
 import {ErrorMessage} from '@clerotri/components/ErrorMessage';
 import {LoadingScreen} from '@clerotri/components/views/LoadingScreen';
-import {storage} from '@clerotri/lib/storage';
-import {migrateToMMKV} from '@clerotri/lib/storage/migration';
 import {initialiseSettings} from '@clerotri/lib/storage/utils';
 import {themes, Theme, ThemeContext} from '@clerotri/lib/themes';
 
 export const App = () => {
-  const [checkedMigration, setCheckedMigration] = useState(false);
   const [theme, setTheme] = useState<Theme>(themes.Dark);
 
   const localStyles = generateLocalStyles(theme);
@@ -26,32 +23,14 @@ export const App = () => {
     setTheme(newTheme);
   });
 
-  useEffect(() => {
-    async function migrationCheck() {
-      const hasMigrated = storage.getBoolean('hasMigrated');
-
-      if (!hasMigrated) {
-        await migrateToMMKV();
-      }
-
-      initialiseSettings();
-
-      setCheckedMigration(true);
-    }
-
-    migrationCheck();
-  }, []);
+  initialiseSettings();
 
   return (
     <GestureHandlerRootView style={localStyles.outer}>
       <ThemeContext.Provider
         value={{currentTheme: theme, setCurrentTheme: setTheme}}>
         <ErrorBoundary fallbackRender={ErrorMessage}>
-          {checkedMigration ? (
-            <MainView />
-          ) : (
-            <LoadingScreen header={'app.loading.migrating_settings'} />
-          )}
+          <MainView />
         </ErrorBoundary>
       </ThemeContext.Provider>
     </GestureHandlerRootView>
