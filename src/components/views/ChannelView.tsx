7d4a61a13cce8583f51a5e71946ee09822aadbdb
Rexogamer <beartechtalks@gmail.com>
2025-01-27 12:44:59 +0000

    refactor: split ChannelView into separate components

--- src/components/views/ChannelView.tsx
+++ src/components/views/ChannelView.tsx
@@ -50,15 +50,155 @@ function MessageViewErrorMessage({
   );
 }
 
-type CVChannel = Channel | 'friends' | 'discover' | 'debug' | null;
+type SpecialChannel = 'friends' | 'discover' | 'debug' | null;
 
-export const ChannelView = observer(({channel}: {channel: CVChannel}) => {
+type CVChannel = Channel | SpecialChannel;
+
+const SpecialChannelViews = observer(({channel}: {channel: SpecialChannel}) => {
+  return channel === 'friends' ? (
+    <FriendsPage />
+  ) : channel === 'discover' ? (
+    <DiscoverPage />
+  ) : channel === 'debug' ? (
+    <View style={styles.flex}>
+      <ChannelHeader
+        icon={<SpecialChannelIcon channel={'Debug'} />}
+        name={'Debug Menu'}
+      />
+      <Text type={'h1'}>howdy</Text>
+    </View>
+  ) : (
+    <HomePage />
+  );
+});
+
+const RegularChannelView = observer(({channel}: {channel: Channel}) => {
   const {currentTheme} = useContext(ThemeContext);
-  const localStyles = generateLocalStyles(currentTheme);
 
   const [renderCount, rerender] = useState(0);
 
   const handledMessages = [] as string[];
+
+  return (
+    <View style={styles.flex}>
+      <ChannelHeader
+        icon={
+          channel.channel_type === 'SavedMessages' ? (
+            <SpecialChannelIcon channel={'Saved Notes'} />
+          ) : (
+            <ChannelIcon channel={channel} />
+          )
+        }
+        name={
+          channel.channel_type === 'DirectMessage'
+            ? channel.recipient?.username
+            : channel.channel_type === 'SavedMessages'
+              ? 'Saved Notes'
+              : (channel.name ?? '')
+        }>
+        {channel.channel_type !== 'VoiceChannel' ? (
+          <View style={{marginEnd: 16}}>
+            <TouchableOpacity
+              onPress={() => app.openPinnedMessagesMenu(channel)}>
+              <MaterialCommunityIcon
+                name="pin"
+                size={24}
+                color={currentTheme.foregroundPrimary}
+              />
+            </TouchableOpacity>
+          </View>
+        ) : null}
+        {channel.channel_type === 'Group' || channel.server ? (
+          <View style={{marginEnd: 16}}>
+            <TouchableOpacity
+              onPress={() => app.openChannelContextMenu(channel)}>
+              <MaterialIcon
+                name="info"
+                size={24}
+                color={currentTheme.foregroundPrimary}
+              />
+            </TouchableOpacity>
+          </View>
+        ) : null}
+        {channel.channel_type === 'Group' ? (
+          <View style={{marginEnd: 16}}>
+            <TouchableOpacity
+              onPress={() => {
+                app.openMemberList(channel);
+              }}>
+              <MaterialIcon
+                name="group"
+                size={24}
+                color={currentTheme.foregroundPrimary}
+              />
+            </TouchableOpacity>
+          </View>
+        ) : null}
+      </ChannelHeader>
+      {channel?.channel_type === 'VoiceChannel' ? (
+        <VoiceChannel />
+      ) : !channel?.nsfw || app.settings.get('ui.messaging.showNSFWContent') ? (
+        <ErrorBoundary fallbackRender={MessageViewErrorMessage}>
+          {app.settings.get('ui.messaging.useNewMessageView') ? (
+            <NewMessageView
+              channel={channel}
+              handledMessages={handledMessages}
+            />
+          ) : (
+            <>
+              <Messages
+                channel={channel}
+                onLongPress={m => {
+                  app.openMessage(m);
+                }}
+                onUserPress={m => {
+                  app.openProfile(m.author, channel.server);
+                }}
+                onUsernamePress={m => {
+                  const currentText = app.getMessageBoxInput();
+                  app.setMessageBoxInput(
+                    `${currentText}${
+                      currentText.length > 0 ? ' ' : ''
+                    }<@${m.author?._id}>`,
+                  );
+                }}
+              />
+              <MessageBox channel={channel} />
+            </>
+          )}
+        </ErrorBoundary>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 25,
+          }}>
+          <Text style={{fontWeight: 'bold', fontSize: 28}}>Hold it!</Text>
+          <Text style={{textAlign: 'center', fontSize: 16}}>
+            This is an NSFW channel. Are you sure you want to enter?
+            {'\n'}
+            (This can be reversed in Settings.)
+          </Text>
+          <Button
+            onPress={async () => {
+              app.settings.set('ui.messaging.showNSFWContent', true);
+              rerender(renderCount + 1);
+            }}>
+            <Text style={styles.buttonText}>
+              I am 18 or older and wish to enter
+            </Text>
+          </Button>
+        </View>
+      )}
+    </View>
+  );
+});
+
+export const ChannelView = observer(({channel}: {channel: CVChannel}) => {
+  const {currentTheme} = useContext(ThemeContext);
+  const localStyles = generateLocalStyles(currentTheme);
 
   console.log(
     `[CHANNELVIEW] Rendering channel view for ${
@@ -68,140 +208,10 @@ export const ChannelView = observer(({channel}: {channel: CVChannel}) => {
 
   return (
     <View style={localStyles.mainView}>
-      {channel ? (
-        channel === 'friends' ? (
-          <FriendsPage />
-        ) : channel === 'discover' ? (
-          <DiscoverPage />
-        ) : channel === 'debug' ? (
-          <View style={styles.flex}>
-            <ChannelHeader
-              icon={<SpecialChannelIcon channel={'Debug'} />}
-              name={'Debug Menu'}
-            />
-            <Text type={'h1'}>howdy</Text>
-          </View>
-        ) : (
-          <View style={styles.flex}>
-            <ChannelHeader
-              icon={
-                channel.channel_type === 'SavedMessages' ? (
-                  <SpecialChannelIcon channel={'Saved Notes'} />
-                ) : (
-                  <ChannelIcon channel={channel} />
-                )
-              }
-              name={
-                channel.channel_type === 'DirectMessage'
-                  ? channel.recipient?.username
-                  : channel.channel_type === 'SavedMessages'
-                  ? 'Saved Notes'
-                  : channel.name ?? ''
-              }>
-              {channel.channel_type !== 'VoiceChannel' ? (
-                <View style={{marginEnd: 16}}>
-                  <TouchableOpacity
-                    onPress={async () => app.openPinnedMessagesMenu(channel)}>
-                    <MaterialCommunityIcon
-                      name="pin"
-                      size={24}
-                      color={currentTheme.foregroundPrimary}
-                    />
-                  </TouchableOpacity>
-                </View>
-              ) : null}
-              {channel.channel_type === 'Group' || channel.server ? (
-                <View style={{marginEnd: 16}}>
-                  <TouchableOpacity
-                    onPress={async () => app.openChannelContextMenu(channel)}>
-                    <MaterialIcon
-                      name="info"
-                      size={24}
-                      color={currentTheme.foregroundPrimary}
-                    />
-                  </TouchableOpacity>
-                </View>
-              ) : null}
-              {channel.channel_type === 'Group' ? (
-                <View style={{marginEnd: 16}}>
-                  <TouchableOpacity
-                    onPress={() => {
-                      app.openMemberList(channel);
-                    }}>
-                    <MaterialIcon
-                      name="group"
-                      size={24}
-                      color={currentTheme.foregroundPrimary}
-                    />
-                  </TouchableOpacity>
-                </View>
-              ) : null}
-            </ChannelHeader>
-            {channel?.channel_type === 'VoiceChannel' ? (
-              <VoiceChannel />
-            ) : !channel?.nsfw ||
-              app.settings.get('ui.messaging.showNSFWContent') ? (
-              <ErrorBoundary fallbackRender={MessageViewErrorMessage}>
-                {app.settings.get('ui.messaging.useNewMessageView') ? (
-                  <NewMessageView
-                    channel={channel}
-                    handledMessages={handledMessages}
-                  />
-                ) : (
-                  <>
-                    <Messages
-                      channel={channel}
-                      onLongPress={async m => {
-                        app.openMessage(m);
-                      }}
-                      onUserPress={m => {
-                        app.openProfile(m.author, channel.server);
-                      }}
-                      onUsernamePress={m => {
-                        const currentText = app.getMessageBoxInput();
-                        app.setMessageBoxInput(
-                          `${currentText}${
-                            currentText.length > 0 ? ' ' : ''
-                          }<@${m.author?._id}>`,
-                        );
-                      }}
-                    />
-                    <MessageBox channel={channel} />
-                  </>
-                )}
-              </ErrorBoundary>
-            ) : (
-              <View
-                style={{
-                  flex: 1,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  padding: 25,
-                }}>
-                <Text style={{fontWeight: 'bold', fontSize: 28}}>Hold it!</Text>
-                <Text style={{textAlign: 'center', fontSize: 16}}>
-                  This is an NSFW channel. Are you sure you want to enter?
-                  {'\n'}
-                  (This can be reversed in Settings.)
-                </Text>
-                <Button
-                  onPress={async () => {
-                    await app.settings.set(
-                      'ui.messaging.showNSFWContent',
-                      true,
-                    );
-                    rerender(renderCount + 1);
-                  }}>
-                  <Text style={styles.buttonText}>
-                    I am 18 or older and wish to enter
-                  </Text>
-                </Button>
-              </View>
-            )}
-          </View>
-        )
+      {!channel || typeof channel === 'string' ? (
+        <SpecialChannelViews channel={channel} />
       ) : (
-        <HomePage />
+        <RegularChannelView channel={channel} />
       )}
     </View>
   );
