ff60c39d7fc5dcfaf7b325d26c10a57c752c1a74
Rexogamer <beartechtalks@gmail.com>
2025-01-31 21:22:06 +0000

    chore: upgrade to RN v0.77

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,24 +2,22 @@
  * @format
  */
 
-import 'react-native';
+import React from 'react';
+import ReactTestRenderer from 'react-test-renderer';
 import {App} from '../App';
 
-// Note: import explicitly to use the types shipped with jest.
-import {expect, it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+test('renders correctly', async () => {
+  await ReactTestRenderer.act(() => {
+    ReactTestRenderer.create(<App />);
+  });
+});
 
 /* FIXME: sort out import problems
  * (appear to be problems with ESM modules - looks like we need to set up Jest ESM support somehow? hmmm)
  * and get tests running, then uncomment
  **/
-// it('renders correctly', () => {
-//   renderer.create(<App />);
-// });
 
-it('is skipped', () => {
-  console.warn('Skipping App.test.tsx for now - see file comments');
-  expect(true).toBe(true);
-});
+// it('is skipped', () => {
+//   console.warn('Skipping App.test.tsx for now - see file comments');
+//   expect(true).toBe(true);
+// });
